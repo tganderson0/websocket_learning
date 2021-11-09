0c3f8ae5a6e4acbcbb2a7d5128185ca5294a899f
gaseoustortoise <tganderson0@gmail.com>
2021-11-08 23:26:19 -0700

    Initial server and clients are working

---
+++ README.md
@@ -0,0 +1,4 @@
+# Websocket Learning
+
+This is just some simple projects in which I am learning more about websockets and how to use them. The first projects are using the websockets library from python, although I may look into the C# and java versions as well as I go on.
+
---
+++ websocket_practice.py
@@ -0,0 +1,17 @@
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"<<< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f">>> {greeting}")
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765):
+        await asyncio.Future()
+
+asyncio.run(main())
